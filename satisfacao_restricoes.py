db3a606d06fc134161dcc84f07291f2dcb5d6c33
Rafa Lourenço <52457167+Rafasputnick@users.noreply.github.com>
2022-04-10 16:44:19 -0300

    ajustes

--- satisfacao_restricoes.py
+++ satisfacao_restricoes.py
@@ -1,3 +1,6 @@
+from os import remove
+
+
 class Restricao():
     def __init__(self, variaveis):
         self.variaveis = variaveis
@@ -9,6 +12,7 @@ class SatisfacaoRestricoes():
   def __init__(self, variaveis, dominios):
     self.variaveis = variaveis # Variáveis para serem restringidas
     self.dominios = dominios # Domínio de cada variável
+    self.partidas = self.dominios[self.variaveis[0]].copy()
     self.restricoes = {}
     for variavel in self.variaveis:
         self.restricoes[variavel] = []
@@ -28,6 +32,16 @@ class SatisfacaoRestricoes():
         return False
     return True
   
+  def reduzir_dominio(self, atribuicao):
+    dominio_filtrado = self.partidas.copy()
+    for partida1 in self.partidas:
+      for partida2 in atribuicao.values():
+        if (partida1[0] == partida2[0] and partida1[1] == partida2[1]) and partida1 in dominio_filtrado:
+          dominio_filtrado.remove(partida1)
+    print(len(dominio_filtrado))
+    for variavel in self.variaveis:
+      self.dominios[variavel] = dominio_filtrado
+
   def busca_backtracking(self, atribuicao = {}):
     # retorna sucesso quando todas as variáveis forem atribuídas
     if len(atribuicao) == len(self.variaveis):
@@ -42,6 +56,7 @@ class SatisfacaoRestricoes():
       atribuicao_local[primeira_variavel] = valor
       # estamos consistentes, seguir recursão
       if self.esta_consistente(primeira_variavel, atribuicao_local):
+        self.reduzir_dominio(atribuicao_local)
         resultado  = self.busca_backtracking(atribuicao_local)
         # para o backtracking se não encontra todos os resultados
         if resultado is not None:
